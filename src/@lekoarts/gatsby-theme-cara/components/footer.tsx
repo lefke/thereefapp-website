173dd530d36e81ff774ecfca4e852f14e2c6bf63
lefke <ailurus@gmail.com>
2022-01-17 20:59:44 +0000

    change footer content

--- src/@lekoarts/gatsby-theme-cara/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -31,26 +31,10 @@ const Footer = () => {
         }}
       >
         {isDark ? (
-          <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2-light_w30.png" alt="LekoArts Logo" />
+          <img width="60" height="60" src="/icon-logo-dark.png" alt="The Reef App Logo" />
         ) : (
-          <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2_w30.png" alt="LekoArts Logo" />
+          <img width="60" height="60" src="/icon-logo-dark.png" alt="The Reef App Logo" />
         )}
-        {` `}
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          sx={{ ml: 2 }}
-          href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara"
-        >
-          Theme change me  please please
-        </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de?utm_source=cara&utm_medium=Theme"
-        >
-          LekoArtsTestadfdfg
-        </Link>
       </Flex>
     </Box>
   )
